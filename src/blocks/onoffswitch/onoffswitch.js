c268261c72aa9cda60eb95ba246d910f31301f03
letulip <ivladimirskiy@ya.ru>
2020-01-11 20:26:13 +0300

    fix: inverse product images and bg styles

--- src/blocks/onoffswitch/onoffswitch.js
+++ src/blocks/onoffswitch/onoffswitch.js
@@ -1,6 +1,7 @@
 const ONOFFSWITCH_CHECKED = `onoffswitch_checked`;
 const THEME = `.theme`;
 const THEME_COLOR_PROJECT_INVERSE = `theme_color_project-inverse`;
+const THEME_COLOR_PROJECT_DEFAULT = `theme_color_project-default`;
 
 const onoffswitch = body.querySelector(`.onoffswitch`);
 
@@ -8,6 +9,7 @@ const onoffswitchOnClick = () => {
   onoffswitch.classList.toggle(ONOFFSWITCH_CHECKED);
   const theme = onoffswitch.closest(THEME);
   theme.classList.toggle(THEME_COLOR_PROJECT_INVERSE);
+  theme.classList.toggle(THEME_COLOR_PROJECT_DEFAULT);
 }
 
 onoffswitch.addEventListener(`click`, onoffswitchOnClick);
