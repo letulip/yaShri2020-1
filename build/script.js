c281a3c170cdac02473f52d7fb6862036687e2bc
letulip <ivladimirskiy@ya.ru>
2020-01-11 19:09:16 +0300

    feat: add gulp concat functional to project

--- build/script.js
+++ build/script.js
@@ -1,3 +1,5 @@
+const body = document.querySelector(`body`);
+
 const HISTORY__SHOW = `.history__show`;
 const DISPLAY_NONE = `none`;
 const DISPLAY_BLOCK = `block`;
@@ -18,3 +20,17 @@ historShowElements.forEach((element) => {
   element.nextElementSibling.style.display = `none`;
   element.addEventListener(`click`, historyShowOnClick);
 });
+const ONOFFSWITCH_CHECKED = `onoffswitch_checked`;
+const THEME = `.theme`;
+const THEME_COLOR_PROJECT_INVERSE = `theme_color_project-inverse`;
+
+const onoffswitch = body.querySelector(`.onoffswitch`);
+
+const onoffswitchOnClick = () => {
+  onoffswitch.classList.toggle(ONOFFSWITCH_CHECKED);
+  const theme = onoffswitch.closest(THEME);
+  theme.classList.toggle(THEME_COLOR_PROJECT_INVERSE);
+}
+
+onoffswitch.addEventListener(`click`, onoffswitchOnClick);
+//# sourceMappingURL=script.js.map
