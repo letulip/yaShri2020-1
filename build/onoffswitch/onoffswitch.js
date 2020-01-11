593ec9817406588947a146c9eb19e2a81a8ca47d
letulip <ivladimirskiy@ya.ru>
2020-01-11 10:55:56 +0300

    feat: add onoffswitch theme toggle logic, correct styles

--- build/onoffswitch/onoffswitch.js
+++ build/onoffswitch/onoffswitch.js
@@ -1,13 +1,17 @@
 (function () {
   'use strict';
 
-  const onoffswitch_checked = `onoffswitch_checked`;
+  const ONOFFSWITCH_CHECKED = `onoffswitch_checked`;
+  const THEME = `.theme`;
+  const THEME_COLOR_PROJECT_INVERSE = `theme_color_project-inverse`;
 
   const body = document.querySelector(`body`);
   const onoffswitch = body.querySelector(`.onoffswitch`);
 
   const onoffswitchOnClick = () => {
-    onoffswitch.classList.toggle(onoffswitch_checked);
+    onoffswitch.classList.toggle(ONOFFSWITCH_CHECKED);
+    const theme = onoffswitch.closest(THEME);
+    theme.classList.toggle(THEME_COLOR_PROJECT_INVERSE);
   };
 
   onoffswitch.addEventListener(`click`, onoffswitchOnClick);
